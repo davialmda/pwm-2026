bb694d9fc9b6867bf61d685d098d40725abce746
Davi Almeida <davi.00000854557@unicap.br>
2026-08-28 23:10:34 +0000

    corrigindo

--- atividade-01/app/page.js
+++ atividade-01/app/page.js
@@ -1,5 +1,27 @@
 export default function Home() {
   return (
-    <h1>Meus exercícios de CSS</h1>
+    <main>
+      <h1>Meus exercícios de CSS</h1>
+
+      <nav>
+        <ul>
+          <li>
+            <a href="/atividade1">Exercício 1</a>
+          </li>
+
+          <li>
+            <a href="/atividade2">Exercício 2</a>
+          </li>
+
+          <li>
+            <a href="/atividade3">Exercício 3</a>
+          </li>
+
+          <li>
+            <a href="/atividade4">Exercício 4</a>
+          </li>
+        </ul>
+      </nav>
+    </main>
   );
-} 
+}
